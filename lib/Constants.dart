613af4562da5a847fa333bcd9afb0bb89da9bf2c
Rambalaji <balajiramnk@gmail.com>
2021-01-18 13:16:41 +0530

    first commit

---
+++ lib/Constants.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/material.dart';
+
+Color backgroundColor = Color(0xFF1A1A1A);
+Color secondaryColor = Color(0xFFFF7357);
+Color secondaryDark = Color(0xFFFC5956);
+Color iconColor = Color(0xFFE62622);
